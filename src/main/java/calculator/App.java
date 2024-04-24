d87ef9379594f59dee64770e71843bdb996a11c8
Robbie <robbie331@>
2024-04-24 17:47:42 +0900

    get an operator

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class App {
 
   public static void main(String[] args) {
+    char op;
     int x, y;
     Scanner sc = new Scanner(System.in);
 
@@ -12,6 +13,8 @@ public class App {
     x = sc.nextInt();
     System.out.print("두 번째 숫자를 입력하세요: ");
     y = sc.nextInt();
+    System.out.print("사칙연산 기호를 입력하세요: ");
+    op = sc.next().charAt(0);
     sc.close();
   }
 }
