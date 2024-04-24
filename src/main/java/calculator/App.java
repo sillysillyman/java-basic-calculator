49df343c87fe1905f8976692b75674db39400d97
Robbie <robbie331@>
2024-04-24 17:46:26 +0900

    get two integers as the inputs

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,5 +4,14 @@ import java.util.Scanner;
 
 public class App {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    int x, y;
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("첫 번째 숫자를 입력하세요: ");
+    x = sc.nextInt();
+    System.out.print("두 번째 숫자를 입력하세요: ");
+    y = sc.nextInt();
+    sc.close();
+  }
 }
