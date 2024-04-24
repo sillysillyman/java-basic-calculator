44417a82d4ebc3b8e78e75d1bb17174b4737a209
Robbie <robbie331@>
2024-04-24 18:20:14 +0900

    print out the result of the arithmetic expression

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,14 +7,34 @@ public class App {
   public static void main(String[] args) {
     char op;
     int x, y;
+    double result;
     Scanner sc = new Scanner(System.in);
 
-    System.out.print("첫 번째 숫자를 입력하세요: ");
+    System.out.print("Enter the first number: ");
     x = sc.nextInt();
-    System.out.print("두 번째 숫자를 입력하세요: ");
+    System.out.print("Enter the second number: ");
     y = sc.nextInt();
-    System.out.print("사칙연산 기호를 입력하세요: ");
+    System.out.print("Enter the operator: ");
     op = sc.next().charAt(0);
+    result =
+      switch (op) {
+        case '+' -> x + y;
+        case '-' -> x - y;
+        case '*' -> x * y;
+        case '/' -> {
+          if (y != 0) {
+            yield (double) x / y;
+          } else {
+            System.out.println("Division by zero is undefined.");
+            yield Double.NaN;
+          }
+        }
+        default -> {
+          System.out.println(op + " is not an operator.");
+          yield Double.NaN;
+        }
+      };
+    System.out.println("result: " + result);
     sc.close();
   }
 }
