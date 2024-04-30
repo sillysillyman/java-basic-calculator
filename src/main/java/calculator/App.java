0a1690a9041b52b56849915b0ec78c96380a7d5d
sillysillyman <sillysillyman.cs@gmail.com>
2024-04-30 14:35:55 +0900

    add loop

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,33 +4,41 @@ import java.util.Scanner;
 
 public class App {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the first number: ");
-    int x = sc.nextInt();
-    System.out.print("Enter the second number: ");
-    int y = sc.nextInt();
-    System.out.print("Enter the operator: ");
-    char op = sc.next().charAt(0);
-    double result =
-      switch (op) {
-        case '+' -> x + y;
-        case '-' -> x - y;
-        case '*' -> x * y;
-        case '/' -> {
-          if (y != 0) {
-            yield (double) x / y;
-          } else {
-            System.out.println("Division by zero is undefined.");
-            yield Double.NaN;
-          }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter the first number: ");
+            int x = Integer.parseInt(sc.nextLine());
+            System.out.print("Enter the second number: ");
+            int y = Integer.parseInt(sc.nextLine());
+            System.out.print("Enter the operator: ");
+            char op = sc.nextLine().charAt(0);
+            double result = switch (op) {
+                case '+' -> x + y;
+                case '-' -> x - y;
+                case '*' -> x * y;
+                case '/' -> {
+                    if (y != 0) {
+                        yield (double) x / y;
+                    } else {
+                        System.out.println("Division by zero is undefined.");
+                        yield Double.NaN;
+                    }
+                }
+                default -> {
+                    System.out.println(op + " is not an operator.");
+                    yield Double.NaN;
+                }
+            };
+            System.out.println("result: " + result);
+            System.out.println(
+                "Do you want to calculate more? (Press ENTER to continue, type 'exit' to end)");
+
+            String input = sc.nextLine();
+            if (input.strip().equalsIgnoreCase("exit")) {
+                break;
+            }
         }
-        default -> {
-          System.out.println(op + " is not an operator.");
-          yield Double.NaN;
-        }
-      };
-    System.out.println("result: " + result);
-    sc.close();
-  }
+        sc.close();
+    }
 }
