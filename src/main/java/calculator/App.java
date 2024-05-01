6c0800cf7eda4cab2eb137d4fa6ba41035495422
sillysillyman <sillysillyman.cs@gmail.com>
2024-05-01 12:51:09 +0900

    add calculating circle

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,6 @@
 package calculator;
 
 import java.util.Scanner;
-import java.util.Queue;
-import java.util.LinkedList;
 
 public class App {
 
@@ -11,44 +9,70 @@ public class App {
         Calculator calc = new Calculator();
 
         while (true) {
-            double x = 0;
-            while (true) {
-                try {
-                    System.out.print("Enter the first number: ");
-                    x = Double.parseDouble(sc.nextLine());
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid input. Please enter a valid integer.");
+            System.out.println("\nChoose an option:");
+            System.out.println("1. Arithmetic Operation");
+            System.out.println("2. Calculate Circle Area");
+            int choice = Integer.parseInt(sc.nextLine());
+
+            switch (choice) {
+                case 1 -> {
+                    double x = 0;
+                    while (true) {
+                        try {
+                            System.out.print("Enter the first number: ");
+                            x = Double.parseDouble(sc.nextLine());
+                            break;
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid input. Please enter a valid integer.");
+                        }
+                    }
+
+                    double y = 0;
+                    while (true) {
+                        try {
+                            System.out.print("Enter the second number: ");
+                            y = Double.parseDouble(sc.nextLine());
+                            break;
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid input. Please enter a valid integer.");
+                        }
+                    }
+
+                    System.out.print("Enter the operator (+, -, *, /): ");
+                    char op = sc.nextLine().charAt(0);
+                    double result = calc.calculate(x, y, op);
+                    System.out.println("result: " + result);
+                    System.out.println(
+                        "Do you want to remove the oldest result? (Press ENTER to continue, type 'remove' to delete it)");
+                    String removeMsg = sc.nextLine();
+                    if (removeMsg.strip().equalsIgnoreCase("remove")) {
+                        calc.removeResult();
+                    }
+                    System.out.println(
+                        "Do you want to inquiry the saved results? (Press ENTER to continue, type 'inquiry' to check them)");
+                    String inquiryMsg = sc.nextLine();
+                    if (inquiryMsg.strip().equalsIgnoreCase("inquiry")) {
+                        calc.inquiryResults();
+                    }
                 }
-            }
-
-            double y = 0;
-            while (true) {
-                try {
-                    System.out.print("Enter the second number: ");
-                    y = Double.parseDouble(sc.nextLine());
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid input. Please enter a valid integer.");
+                case 2 -> {
+                    System.out.print("Enter the radius: ");
+                    double radius = Double.parseDouble(sc.nextLine());
+                    double area = calc.calculateCircleArea(radius);
+                    if (calc.getAreas().isEmpty()) {
+                        System.out.println("Results queue is empty.");
+                    } else {
+                        System.out.print("results: [ ");
+                        calc.getAreas().forEach(
+                            element -> {
+                                System.out.printf("%.2f ", element);
+                            });
+                        System.out.println("]");
+                    }
                 }
-            }
-
-            System.out.print("Enter the operator (+, -, *, /): ");
-            char op = sc.nextLine().charAt(0);
-
-            double result = calc.calculate(x, y, op);
-            System.out.println("result: " + result);
-            System.out.println(
-                "Do you want to remove the oldest result? (Press ENTER to continue, type 'remove' to delete it)");
-            String removeMsg = sc.nextLine();
-            if (removeMsg.strip().equalsIgnoreCase("remove")) {
-                calc.removeResult();
-            }
-            System.out.println(
-                "Do you want to inquiry the saved results? (Press ENTER to continue, type 'inquiry' to check them)");
-            String inquiryMsg = sc.nextLine();
-            if (inquiryMsg.strip().equalsIgnoreCase("inquiry")) {
-                calc.inquiryResults();
+                default -> {
+                    throw new IllegalArgumentException("Unsupported option: " + choice);
+                }
             }
             System.out.println(
                 "Do you want to calculate more? (Press ENTER to continue, type 'exit' to end)");
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,40 @@
 package calculator;
 
+import java.lang.Math;
 import java.util.Queue;
 import java.util.LinkedList;
 
 public class Calculator {
 
+    static final double PI = Math.PI;
     private Queue<Double> results;
+    private Queue<Double> areas;
 
     public Calculator() {
         this.results = new LinkedList<Double>();
+        this.areas = new LinkedList<Double>();
+    }
+
+    public double calculate(double x, double y, char op) {
+        double result = switch (op) {
+            case '+' -> x + y;
+            case '-' -> x - y;
+            case '*' -> x * y;
+            case '/' -> {
+                if (y != 0) {
+                    yield x / y;
+                } else {
+                    throw new ArithmeticException("Division by zero is undefined.");
+                }
+            }
+            default -> {
+                throw new IllegalArgumentException("Unsupported operation: " + op);
+            }
+        };
+        if (!results.offer(result)) {
+            System.out.println("Enqueue operation failed.");
+        }
+        return result;
     }
 
     public Queue<Double> getResults() {
@@ -38,25 +64,19 @@ public class Calculator {
         }
     }
 
-    public double calculate(double x, double y, char op) {
-        double result = switch (op) {
-            case '+' -> x + y;
-            case '-' -> x - y;
-            case '*' -> x * y;
-            case '/' -> {
-                if (y != 0) {
-                    yield x / y;
-                } else {
-                    throw new ArithmeticException("Division by zero is undefined.");
-                }
-            }
-            default -> {
-                throw new IllegalArgumentException("Unsupported operation: " + op);
-            }
-        };
-        if (!results.offer(result)) {
+    public double calculateCircleArea(double radius) {
+        double area = PI * radius * radius;
+        if (!areas.offer(area)) {
             System.out.println("Enqueue operation failed.");
         }
-        return result;
+        return area;
+    }
+
+    public Queue<Double> getAreas() {
+        return new LinkedList<>(areas);
+    }
+
+    public void setAreas(Queue<Double> areas) {
+        this.areas = areas;
     }
 }
